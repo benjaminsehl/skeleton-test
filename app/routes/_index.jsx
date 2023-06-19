a7e21bbe179ee50cb45317053686b543ea41bc41
Benjamin Sehl <ben.sehl@shopify.com>
2023-06-19 19:57:43 -0400

    Adds collections index

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,27 @@
-import {useRouteError, isRouteErrorResponse} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
+import {Image} from '@shopify/hydrogen';
+import {
+  useLoaderData,
+  Link,
+  useRouteError,
+  isRouteErrorResponse,
+} from '@remix-run/react';
+
+export async function loader({context}) {
+  const {collection} = await context.storefront.query(HOME_QUERY, {
+    variables: {
+      handle: 'freestyle',
+    },
+  });
+
+  if (!collection) {
+    throw new Response('Not Found', {
+      status: 404,
+    });
+  }
+
+  return json({collection});
+}
 
 /**
  * @TODO: Homepage
@@ -6,10 +29,32 @@ import {useRouteError, isRouteErrorResponse} from '@remix-run/react';
  * - Display top 8 products in a grid
  */
 export default function Index() {
+  const {collection} = useLoaderData();
   return (
-    <p>
-      Edit this route in <em>app/routes/index.tsx</em>.
-    </p>
+    <>
+      <section>
+        <Link to={`/collections/${collection.handle}`}>
+          <Image
+            style={{height: 'auto'}} // This shouldn't be required
+            aspectRatio="16/9"
+            data={collection.image}
+          />
+          <h1>{collection.title}</h1>
+        </Link>
+      </section>
+      <section>
+        {collection.products.nodes.map((product) => (
+          <div key={product.id}>
+            <Image
+              style={{height: 'auto'}} // This shouldn't be required
+              aspectRatio="4/5"
+              data={product.featuredImage}
+            />
+            <h3>{product.title}</h3>
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
 
@@ -24,3 +69,37 @@ export function ErrorBoundary() {
     return <div>Thrown Error</div>;
   }
 }
+
+const HOME_QUERY = `#graphql
+  query home(
+    $handle: String!
+    $country: CountryCode
+    $language: LanguageCode
+  ) @inContext(country: $country, language: $language) {
+    collection(handle: $handle) {
+      id
+      handle
+      title
+      description
+      image {
+        altText
+        height
+        width
+        url
+      }
+      products(first: 8) {
+        nodes {
+          title
+          id
+          handle
+          featuredImage {
+            altText
+            height
+            width
+            url
+          }
+        }
+      }
+    }
+  }
+  `;
--- app/routes/collections._index.jsx
+++ app/routes/collections._index.jsx
@@ -3,3 +3,95 @@
  * will have a title, and a link to the collection
  * p2
  **/
+
+import {json} from '@shopify/remix-oxygen';
+import {
+  useLoaderData,
+  Link,
+  useRouteError,
+  isRouteErrorResponse,
+} from '@remix-run/react';
+import {
+  Pagination__unstable as Pagination,
+  getPaginationVariables__unstable as getPaginationVariables,
+} from '@shopify/hydrogen';
+
+export async function loader({context, request}) {
+  const paginationVariables = getPaginationVariables(request, {pageBy: 8});
+
+  const {collections} = await context.storefront.query(COLLECTIONS_QUERY, {
+    variables: {
+      ...paginationVariables,
+    },
+  });
+
+  if (!collections) {
+    throw new Response(null, {status: 404});
+  }
+
+  return json({collections});
+}
+
+export default function Collections() {
+  const {collections} = useLoaderData();
+
+  return (
+    <>
+      <h1>Collections</h1>
+      <Pagination connection={collections}>
+        {({nodes, PreviousLink, NextLink}) => (
+          <>
+            <PreviousLink>Load previous</PreviousLink>
+            <ul>
+              {nodes.map((product) => (
+                <li key={product.id}>
+                  <Link to={`/products/${product.handle}`}>
+                    {product.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <NextLink>Load more</NextLink>
+          </>
+        )}
+      </Pagination>
+    </>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    console.error(error.status, error.statusText, error.data); // TODO: Remove this eslint rule
+    return <div>Route Error</div>;
+  } else {
+    console.error(error.message);
+    return <div>Thrown Error</div>;
+  }
+}
+
+const COLLECTIONS_QUERY = `#graphql
+  query collection_index(
+    $country: CountryCode
+    $language: LanguageCode
+    $first: Int
+    $last: Int
+    $startCursor: String
+    $endCursor: String
+  ) @inContext(country: $country, language: $language) {
+    collections(first: $first, last: $last, before: $startCursor, after: $endCursor) {
+        nodes {
+            title
+            id
+            handle
+        }
+        pageInfo {
+            hasPreviousPage
+            hasNextPage
+            startCursor
+            endCursor
+        }
+    }
+  }
+`;
